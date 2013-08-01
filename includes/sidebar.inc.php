511d34b1c6ff81712f372d5e8aa9a886d73e87cf
jamietribe <jamietribe@ymail.com>
2013-08-01 13:21:38 -0400

    test
    test

---
+++ includes/sidebar.inc.php
@@ -0,0 +1,6 @@
+<aside>
+  	<p>
+    You can shorten the code by combining both lines into one like this:
+When you nest functions like this, PHP processes the innermost one first and passes the result to the outer function. It makes your code shorter, but it's not so easy to read.
+	</p>
+</aside>
